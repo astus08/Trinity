2d2dd388e1a662601d0c18abe278894b7ff6f31b
Guillaume <guillaume.simonrimbault@viacesi.fr>
2017-04-12 09:07:02 +0200

    1st commit

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+gulp            = require('gulp');
+$               = require('gulp-load-plugins')();
+
+browserSync     = require('browser-sync').create();
+
+var path = {
+    css     : 'css/',
+    scss    : 'css/'
+}
+
+gulp.task('browser_reload', function(done) {
+    browserSync.reload();
+    done();
+});
+
+gulp.task('sass_compilation_+_prefixer', function(){
+    gulp.src(path.scss + '*.scss')
+    .pipe($.sass().on('error', $.sass.logError))
+    .pipe($.autoprefixer())
+    .pipe(gulp.dest(path.css));
+})
+
+
+gulp.task('default', function(){
+    browserSync.init({
+        proxy: "localhost:8080/Trinity"
+    });
+
+    gulp.watch(path.scss + '**/*.scss', ['sass_compilation_+_prefixer']);
+
+    gulp.watch(['*.html', 'js/*.js', '*.php', 'css/*.css'], ['browser_reload']);
+})
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html lang="fr">
+
+    <head>
+        <meta charset="utf-8">
+        <title>Trinity</title>
+        <link rel="stylesheet" href="css/style.css" />
+    </head>
+
+    <body>
+        <p>Salut</p>
+    </body>
+
+    <script type="text/javascript" src="js/app.js"></script>
+</html>
