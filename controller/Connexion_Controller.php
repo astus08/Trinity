6bd035baec73252681b86fe03f03b5e86267c554
Maxia51 <maxence.margaine@gmail.com>
2017-04-13 13:13:20 +0200

    cleaner 2

---
+++ controller/Connexion_Controller.php
@@ -0,0 +1,9 @@
+<?php
+include 'modele/SPDO.php';
+
+var_dump($_POST);
+
+$bdd = SPDO::getInstance();
+
+
+?>
--- view/home.php
+++ view/home.php
@@ -6,5 +6,22 @@
 <body>
 
 <h1>hello home </h1>
+
+<!-- Connexion FORM -->
+<form method="POST" action="Connexion_Controller.php">
+	<input type="email" name="mail" label="email">
+	<input type="password" name="pwd" label="password">
+	<input type="submit" name="btn" value="submit">
+</form>
+
+<!-- Inscription FORM -->
+<form method="POST" action="Connexion_Controller.php">
+	<input type="text" name="lastName" label="lastName">
+	<input type="text" name="firstName" label="firstName">
+	<input type="email" name="mail" label="email">
+	<input type="password" name="pwd" label="password">
+	<input type="submit" name="btn" value="submit">
+</form>
+
 </body>
 </html>
