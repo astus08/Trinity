9dcbc7fc06450b0568e978fb822a61a411ac7887
Guillaume <guillaume.simonrimbault@viacesi.fr>
2017-04-13 16:15:05 +0200

    activities

--- controller/Activity_Controller.php
+++ controller/Activity_Controller.php
@@ -1,7 +1,39 @@
-<?php
+<?php 
+namespace controller;
 
-var_dump($_POST)
+require '..\modele\PDO\SPDO.php';
+use modele\PDO\SPDO;
 
+if (isset($_GET['action'])){
+    switch($_GET['action']){
+        case 'all':
+            articles();
+            break;
+    }
+} else {
+    
+}
+
+function articles(){
+
+    $rows = array();
+
+    foreach (SPDO::getInstance()->query('   SELECT *
+                                            FROM activities
+                                            WHERE vote_enable=0') as $line){
+        $rows[] = array_map('utf8_encode', $line);
+    }
+
+    echo json_encode($rows);
+}
+
+function article($id){
+    foreach (SPDO::getInstance()->query('   SELECT *
+                                            FROM activities
+                                            WHERE id = '.$id) as $line){
+        $rows[] = array_map('utf8_encode', $line);
+    }
+}
 
 
 ?>
