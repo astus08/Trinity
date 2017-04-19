b5b0955674eb8143899228e027175cc0dec20456
t0inou <antoine.hulin@viacesi.fr>
2017-04-19 16:58:12 +0200

    yo

--- view/suggestion.php
+++ view/suggestion.php
@@ -8,11 +8,17 @@ if (!isset($_SESSION['id'])) {
 }
 ?>
 
-<form action="../controller/Suggestion_Controller.php" method="POST">
-	<input type="text" name="id_user" value="<?php echo $_SESSION['id']; ?>" style="display:none;">
-	<textarea name="content" cols="50" rows="10"></textarea>
-	<input type="submit" name="btn">
-</form>
+<head>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<div class="container-suggestion">
+	<form action="../controller/Suggestion_Controller.php" method="POST">
+		<input type="text" name="id_user" value="<?php echo $_SESSION['id']; ?>" style="display:none;">
+		<textarea class="text-box" name="content" cols="50" rows="10"></textarea>
+		<input class="btn-login" value="Ajouter la suggestion" type="submit" name="btn">
+	</form>
+</div>
 
 <?php
 
